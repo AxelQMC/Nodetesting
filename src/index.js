a7aaab3def24df0118f6b55f05d79b7cb5518ce3
AxelQMC <ajgurrero@qmcmex.com>
2021-05-28 11:20:05 -0500

    routes and bootstrap

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Server Works")
+const express = require("express");
+const app = express();
+const path = require("path");
+
+//settings
+app.set("port",3000);
+app.set("views",path.join(__dirname, "views"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "ejs");
+
+//middlewares
+
+//routes
+app.use(require("./routes/index.js"));
+
+//static files
+
+
+//server listening
+app.listen(app.get("port"), () => {
+    console.log("Server on port", app.get("port"));
+});
+
